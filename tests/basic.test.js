addf0cde4379529123c50eeac0c950e69b59ae51
Dobobaie <dobobaie@hotmail.fr>
2020-01-14 00:17:51 +0100

    Feature(Service): update test + fix one situation in gerjs-koa lib

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -1,9 +1,11 @@
+process.env.gerjsKoaRequire = __dirname + "/../lib";
 process.env.NODE_ENV = "development";
 process.env.SERVER_IP = "localhost";
 process.env.SERVER_PORT = 5050;
 process.env.LOCALE = "en";
 process.env.AUTH_TOKEN = "abc";
 
+const fs = require('fs');
 const test = require('ava');
 const request = require('ava-http');
 const gerjsTestKoa = require('gerjs-test-koa');
@@ -21,4 +23,21 @@ test("Server initialization", t =>
   )
   .then(m => t.pass(m))
   .catch(m => t.fail(m))
+);
+
+const swagger_content_file = fs.readFileSync(__dirname + '/../swagger/index.html').toString();
+test("[Request] - Swagger route '/swagger'", t =>
+  request.get(server_url + '/swagger')
+  .then(r =>
+    t.is(r, swagger_content_file, "return a wrong response")
+  )
+  .catch(r => t.fail(r))
+);
+
+test("[Request] - Defaut route '/'", t =>
+  request.get(server_url + '/')
+  .then(r =>
+    t.is(r, "Default reponse", "return a wrong response")
+  )
+  .catch(r => t.fail(r))
 );
